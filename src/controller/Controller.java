1458753ebe0196255470f61242e8b6ce4d1c3fa4
Oz <frost57@gmail.com>
2020-06-24 22:41:18 -0400

    More implementation

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -6,11 +6,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import model.DigiSystem;
+import model.*;
 
+import javax.swing.*;
 import java.io.IOException;
 
 public class Controller {
@@ -21,8 +24,14 @@ public class Controller {
     private TextField userIDTextBox;
     @FXML
     private PasswordField passPassField;
+    @FXML
+    private Button viewUnitsButton;
+    @FXML
+    private Button viewPatientsButton;
+    @FXML
+    private ListView centerLists;
 
-    public void passChecker(ActionEvent event) throws IOException {
+    public void passChecker() throws IOException {
 
         String userID = userIDTextBox.getText();
         String pass = passPassField.getText();
@@ -32,21 +41,42 @@ public class Controller {
 
         if(digiSystem.passChecker(userID, pass)) {
 
-            System.out.println("good pass");
-
-            Stage stage = (Stage) userIDTextBox.getScene().getWindow();
-            stage.close();
+            //System.out.println("good pass");
             loadDigiHealth();
 
         } else {
 
             userIDTextBox.setPromptText("Try again");
 
-            System.out.println("wrong pass");
+            //System.out.println("wrong pass");
         }
     }
 
+    public void listViewAppear(ActionEvent event) {
+
+        centerLists.setVisible(true);
+
+        Unit unit = new AUnit("123", "ER");
+
+        System.out.println(unit.toString());
+        if(event.getSource() == viewUnitsButton) {
+//            centerLists.getItems().add(unit);
+//            centerLists.getItems().add("unit2");
+//            centerLists.getItems().add("unit3");
+        }
+
+        if(event.getSource() == viewPatientsButton) {
+//            centerLists.getItems().add("patient1");
+//            centerLists.getItems().add("patient2");
+//            centerLists.getItems().add("patient3");
+        }
+
+    }
+
     private void loadDigiHealth() throws IOException {
+
+        Stage stage = (Stage) userIDTextBox.getScene().getWindow();
+        stage.close();
 
         Stage primaryStage = new Stage();
         Parent root = FXMLLoader.load(getClass().getResource("../view/DigiHealth.fxml"));
--- src/controller/Debugging.java
+++ src/controller/Debugging.java
@@ -7,7 +7,7 @@ public class Debugging {
     DigiSystem digiSystem =  DigiSystem.getInstance();
     public void debug() {
 
-        AUser user = new AUser("user", "pass");
+        AUser user = new AUser("", "");
         digiSystem.addUser(user);
         AUser user2 = new AUser("admin", "password");
         digiSystem.addUser(user2);
